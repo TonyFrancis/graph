9e3326f3ce981c12a9642f6ae39815cfea50a485
George Tony <gtony@suyati.com>
2017-05-10 10:26:34 +0530

    tests

--- test/test_graph.py
+++ test/test_graph.py
@@ -14,18 +14,18 @@ class TestGraph(unittest.TestCase):
         4. Vertex get added.
         """
         oGraph = Graph()
-        self.assertEqual(oGraph.vertex,[],"Vertex Not Empty")
-        oGraph.addEdge("A","B")
-        self.assertEqual(oGraph.vertex,["A","B"], "Vertex is not added")
-        self.assertEqual(oGraph.edges, { "A" : ["B"]}, "Edge A -> B not saved")
-        oGraph.addEdge("A","A")
-        self.assertEqual(oGraph.vertex,["A","B"],"Vertex is not repeated")
+        self.assertEqual(oGraph.vertex, [], "Vertex Not Empty")
+        oGraph.addEdge("A", "B")
+        self.assertEqual(oGraph.vertex, ["A", "B"], "Vertex is not added")
+        self.assertEqual(oGraph.edges, { "A" : ["B"] }, "Edge A -> B not saved")
+        oGraph.addEdge("A", "A")
+        self.assertEqual(oGraph.vertex, ["A", "B"], "Vertex is not repeated")
         self.assertEqual(oGraph.edges, { "A" : ["B"]}, "Edge A -> A exists")
-        oGraph.addEdge("A","B")
+        oGraph.addEdge("A", "B")
         self.assertEqual(oGraph.edges, { "A" : ["B"]}, "Repeated Entered")
-        oGraph.addEdge("B","C")
+        oGraph.addEdge("B", "C")
         self.assertEqual(oGraph.edges, { "A": ["B"], "B" : ["C"]})
-        self.assertEqual(oGraph.vertex,["A","B", "C"],"vertex not added properly")
+        self.assertEqual(oGraph.vertex, ["A", "B", "C"], "vertex not added properly")
 
     def test_dfs_paths(self):
         """
@@ -35,27 +35,28 @@ class TestGraph(unittest.TestCase):
         2. Starting element is 'u' and ending in 'v'.
         """
         oGraph = Graph()
-        self.assertEqual(list(oGraph.dfs_paths("A","B")),[],"No Path")
-        oGraph.addEdge("A","B")
-        paths = list(oGraph.dfs_paths("A","B"))
-        self.assertEqual(len(paths),1, "Single path from A to B")
-        self.assertEqual(len(paths[0]),2,"length of path is equal to no of vertex")
-        self.assertEqual(paths[0][0],"A","Starts from A")
-        self.assertEqual(paths[0][-1],"B","Ends at B")
-        oGraph.addEdge("A","C")
-        paths = list(oGraph.dfs_paths("A","B"))
-        self.assertEqual(len(paths),1, "Single path from A to B")
-        self.assertEqual(len(paths[0]),3,"length of path is equal to no of vertex")
-        self.assertEqual(paths[0][0],"A","Starts from A")
-        self.assertEqual(paths[0][-1],"B","Ends at B")
-        oGraph.addEdge("A","D")
-        paths = list(oGraph.dfs_paths("A","B"))
-        self.assertEqual(len(paths),2, "Single path from A to B,'ACDB','ADCB'")
-        self.assertEqual(len(paths[0]),4,"length of path is equal to no of vertex")
-        self.assertEqual(paths[0][0],"A","Starts from A")
-        self.assertEqual(paths[0][-1],"B","Ends at B")
+        self.assertEqual(list(oGraph.dfs_paths("A", "B")), [], "No Path")
+        oGraph.addEdge("A", "B")
+        paths = list(oGraph.dfs_paths("A", "B"))
+        self.assertEqual(len(paths), 1, "Single path from A to B")
+        self.assertEqual(len(paths[0]), 2, "length of path is equal to no of vertex")
+        self.assertEqual(paths[0][0], "A", "Starts from A")
+        self.assertEqual(paths[0][-1], "B", "Ends at B")
+        oGraph.addEdge("A", "C")
+        paths = list(oGraph.dfs_paths("A", "B"))
+        self.assertEqual(len(paths), 1, "Single path from A to B")
+        self.assertEqual(len(paths[0]), 3, "length of path is equal to no of vertex")
+        self.assertEqual(paths[0][0], "A", "Starts from A")
+        self.assertEqual(paths[0][-1], "B", "Ends at B")
+        oGraph.addEdge("A", "D")
+        paths = list(oGraph.dfs_paths("A", "B"))
+        self.assertEqual(len(paths), 2, "Single path from A to B,'ACDB','ADCB'")
+        self.assertEqual(len(paths[0]), 4, "length of path is equal to no of vertex")
+        self.assertEqual(paths[0][0], "A", "Starts from A")
+        self.assertEqual(paths[0][-1], "B", "Ends at B")
 
     def check_vertex_at_begin(self, vertex, paths):
+        """Check vertex present at begining of the path"""
         vertex_at_begin = False
         for path in paths:
             if path[0] == vertex:
@@ -64,6 +65,7 @@ class TestGraph(unittest.TestCase):
         return vertex_at_begin
 
     def check_vertex_at_end(self, vertex, paths):
+        """Check vertex present at end of the path"""
         vertex_at_end = False
         for path in paths:
             if path[-1] == vertex:
@@ -72,6 +74,11 @@ class TestGraph(unittest.TestCase):
         return vertex_at_end
 
     def check_path_length(self, paths, length):
+        """
+        check length of all path
+
+        Path length should same as length.
+        """
         for path in paths:
             if len(path) != length:
                 return False
@@ -85,10 +92,10 @@ class TestGraph(unittest.TestCase):
         2. All Paths should have equal length.
         """
         oGraph = Graph()
-        self.assertEqual(oGraph.list_all_paths(),[],"Empty list")
+        self.assertEqual(oGraph.list_all_paths(), [], "Empty list")
         oGraph.addEdge("A", "B")
         paths = oGraph.list_all_paths()
-        self.assertEqual(len(paths),2,"there are 2 path.since only 2 vertex")
+        self.assertEqual(len(paths), 2, "there are 2 path.since only 2 vertex")
         self.assertTrue(self.check_vertex_at_begin("A", paths), "vertex A present in Begining ")
         self.assertTrue(self.check_vertex_at_begin("B", paths), "vertex B present in Begining ")
         self.assertTrue(self.check_vertex_at_end("A", paths), "vertex A present in Ending ")
@@ -96,7 +103,7 @@ class TestGraph(unittest.TestCase):
         self.assertTrue(self.check_path_length(paths, len(oGraph.vertex)))
         oGraph.addEdge("A", "C")
         paths = oGraph.list_all_paths()
-        self.assertEqual(len(paths),6,"there are 6 possible path")
+        self.assertEqual(len(paths), 6, "there are 6 possible path")
         self.assertTrue(self.check_vertex_at_begin("A", paths), "vertex A present in Begining ")
         self.assertTrue(self.check_vertex_at_begin("B", paths), "vertex B present in Begining ")
         self.assertTrue(self.check_vertex_at_begin("C", paths), "vertex C present in Begining ")
