16dda87d98ee0fc51e5d25cbad00f0a0359a5468
George Tony <gtony@suyati.com>
2017-05-10 10:26:54 +0530

    tests

--- app.py
+++ app.py
@@ -5,12 +5,4 @@ if __name__ == "__main__":
     oGraph.addEdge("A","B")
     oGraph.addEdge("B","C")
     oGraph.addEdge("A","C")
-    # print oGraph.edges
-    # print list(oGraph.dfs_paths("A","B"))
     print oGraph.list_all_paths()
-    # print list(oGraph.dfs_paths("B","A"))
-    # print list(oGraph.dfs_paths("A","B"))
-    # print list(oGraph.dfs_paths("A","C"))
-    # print list(oGraph.dfs_paths("B","C"))
-    # print list(oGraph.dfs_paths("C","A"))
-    # print list(oGraph.dfs_paths("C","B"))
